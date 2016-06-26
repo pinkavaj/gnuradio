4bb7b69066907d4a9e27fef34161dafdef05681f
Jiří Pinkava <j-pi@seznam.cz>
2016-06-26 13:04:39 +0200

    gr-digital/python/digital/ofdm.py

--- gr-digital/python/digital/ofdm.py
+++ gr-digital/python/digital/ofdm.py
@@ -100,8 +100,8 @@ class ofdm_mod(gr.hier_block2):
         #print rotated_const
         self._pkt_input = digital.ofdm_mapper_bcv(rotated_const,
                                                   msgq_limit,
-                                                  options.occupied_tones,
-                                                  options.fft_length)
+                                                  args.occupied_tones,
+                                                  args.fft_length)
         
         self.preambles = digital.ofdm_insert_preamble(self._fft_length,
                                                       padded_preambles)
@@ -114,15 +114,15 @@ class ofdm_mod(gr.hier_block2):
         self.connect((self._pkt_input, 1), (self.preambles, 1))
         self.connect(self.preambles, self.ifft, self.cp_adder, self.scale, self)
         
-        if options.verbose:
+        if args.verbose:
             self._print_verbage()
 
-        if options.log:
-            self.connect(self._pkt_input, blocks.file_sink(gr.sizeof_gr_complex*options.fft_length,
+        if args.log:
+            self.connect(self._pkt_input, blocks.file_sink(gr.sizeof_gr_complex*args.fft_length,
                                                        "ofdm_mapper_c.dat"))
-            self.connect(self.preambles, blocks.file_sink(gr.sizeof_gr_complex*options.fft_length,
+            self.connect(self.preambles, blocks.file_sink(gr.sizeof_gr_complex*args.fft_length,
                                                       "ofdm_preambles.dat"))
-            self.connect(self.ifft, blocks.file_sink(gr.sizeof_gr_complex*options.fft_length,
+            self.connect(self.ifft, blocks.file_sink(gr.sizeof_gr_complex*args.fft_length,
                                                  "ofdm_ifft_c.dat"))
             self.connect(self.cp_adder, blocks.file_sink(gr.sizeof_gr_complex,
                                                      "ofdm_cp_adder_c.dat"))
@@ -147,17 +147,17 @@ class ofdm_mod(gr.hier_block2):
         self._pkt_input.msgq().insert_tail(msg)
 
     @staticmethod
-    def add_options(normal, expert):
+    def add_arguments(normal, expert):
         """
         Adds OFDM-specific options to the Options Parser
         """
-        normal.add_option("-m", "--modulation", type="string", default="bpsk",
+        normal.add_option("-m", "--modulation", default="bpsk",
                           help="set modulation type (bpsk, qpsk, 8psk, qam{16,64}) [default=%default]")
-        expert.add_option("", "--fft-length", type="intx", default=512,
+        expert.add_option("", "--fft-length", type=intx, default=512,
                           help="set the number of FFT bins [default=%default]")
-        expert.add_option("", "--occupied-tones", type="intx", default=200,
+        expert.add_option("", "--occupied-tones", type=intx, default=200,
                           help="set the number of occupied FFT bins [default=%default]")
-        expert.add_option("", "--cp-length", type="intx", default=128,
+        expert.add_option("", "--cp-length", type=intx, default=128,
                           help="set the number of bits in the cyclic prefix [default=%default]")
 
     def _print_verbage(self):
@@ -199,11 +199,11 @@ class ofdm_demod(gr.hier_block2):
 
         self._rcvd_pktq = gr.msg_queue()          # holds packets from the PHY
 
-        self._modulation = options.modulation
-        self._fft_length = options.fft_length
-        self._occupied_tones = options.occupied_tones
-        self._cp_length = options.cp_length
-        self._snr = options.snr
+        self._modulation = args.modulation
+        self._fft_length = args.fft_length
+        self._occupied_tones = args.occupied_tones
+        self._cp_length = args.cp_length
+        self._snr = args.snr
 
         # Use freq domain to get doubled-up known symbol for correlation in time domain
         zeros_on_left = int(math.ceil((self._fft_length - self._occupied_tones)/2.0))
@@ -220,7 +220,7 @@ class ofdm_demod(gr.hier_block2):
                                        self._cp_length,
                                        self._occupied_tones,
                                        self._snr, preambles,
-                                       options.log)
+                                       args.log)
 
         mods = {"bpsk": 2, "qpsk": 4, "8psk": 8, "qam8": 8, "qam16": 16, "qam64": 64, "qam256": 256}
         arity = mods[self._modulation]
@@ -253,7 +253,7 @@ class ofdm_demod(gr.hier_block2):
         # thing to export, anyway
         self.connect(self.ofdm_recv.chan_filt, self)
 
-        if options.log:
+        if args.log:
             self.connect(self.ofdm_demod,
                          blocks.file_sink(gr.sizeof_gr_complex*self._occupied_tones,
                                       "ofdm_frame_sink_c.dat"))
@@ -261,25 +261,25 @@ class ofdm_demod(gr.hier_block2):
             self.connect(self.ofdm_demod,
                          blocks.null_sink(gr.sizeof_gr_complex*self._occupied_tones))
 
-        if options.verbose:
+        if args.verbose:
             self._print_verbage()
             
         self._watcher = _queue_watcher_thread(self._rcvd_pktq, callback)
 
     @staticmethod
-    def add_options(normal, expert):
+    def add_arguments(normal, expert):
         """
         Adds OFDM-specific options to the Options Parser
         """
-        normal.add_option("-m", "--modulation", type="string", default="bpsk",
+        normal.add_option("-m", "--modulation", default="bpsk",
                           help="set modulation type (bpsk or qpsk) [default=%default]")
-        expert.add_option("", "--fft-length", type="intx", default=512,
+        expert.add_option("", "--fft-length", type=intx, default=512,
                           help="set the number of FFT bins [default=%default]")
-        expert.add_option("", "--occupied-tones", type="intx", default=200,
+        expert.add_option("", "--occupied-tones", type=intx, default=200,
                           help="set the number of occupied FFT bins [default=%default]")
-        expert.add_option("", "--cp-length", type="intx", default=128,
+        expert.add_option("", "--cp-length", type=intx, default=128,
                           help="set the number of bits in the cyclic prefix [default=%default]")
-        expert.add_option("", "--snr", type="float", default=30.0,
+        expert.add_option("", "--snr", type=float, default=30.0,
                           help="SNR estimate [default=%default]")
 
     def _print_verbage(self):
