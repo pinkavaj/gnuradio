b79b5f80bdc5304d32e6dd2a2ec53135db09499d
Jiří Pinkava <j-pi@seznam.cz>
2016-06-26 13:04:37 +0200

    digital: benchmark_add_channel - replace OptionParser by ArgumentParser

--- gr-digital/examples/narrowband/benchmark_add_channel.py
+++ gr-digital/examples/narrowband/benchmark_add_channel.py
@@ -23,27 +23,27 @@
 from gnuradio import channels, gr
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio.eng_option import eng_option
-from optparse import OptionParser
+from gnuradio.eng_arg import eng_float, intx
+from argparse import ArgumentParser
 
 import random, math, sys
 
 class my_top_block(gr.top_block):
-    def __init__(self, ifile, ofile, options):
+    def __init__(self, ifile, ofile, args):
         gr.top_block.__init__(self)
 
-        SNR = 10.0**(options.snr/10.0)
-        frequency_offset = options.frequency_offset
-        time_offset = options.time_offset
-        phase_offset = options.phase_offset*(math.pi/180.0)
+        SNR = 10.0**(args.snr/10.0)
+        frequency_offset = args.frequency_offset
+        time_offset = args.time_offset
+        phase_offset = args.phase_offset*(math.pi/180.0)
 
         # calculate noise voltage from SNR
-        power_in_signal = abs(options.tx_amplitude)**2
+        power_in_signal = abs(args.tx_amplitude)**2
         noise_power = power_in_signal/SNR
         noise_voltage = math.sqrt(noise_power)
 
         self.src = blocks.file_source(gr.sizeof_gr_complex, ifile)
-        #self.throttle = blocks.throttle(gr.sizeof_gr_complex, options.sample_rate)
+        #self.throttle = blocks.throttle(gr.sizeof_gr_complex, args.sample_rate)
 
         self.channel = channels.channel_model(noise_voltage, frequency_offset,
                                             time_offset, noise_seed=-random.randint(0,100000))
@@ -59,36 +59,31 @@ class my_top_block(gr.top_block):
 # /////////////////////////////////////////////////////////////////////////////
 
 def main():
-    # Create Options Parser:
-    usage = "benchmack_add_channel.py [options] <input file> <output file>"
-    parser = OptionParser (usage=usage, option_class=eng_option, conflict_handler="resolve")
-    parser.add_option("-n", "--snr", type="eng_float", default=30,
-                      help="set the SNR of the channel in dB [default=%default]")
-    parser.add_option("", "--seed", action="store_true", default=False,
-                      help="use a random seed for AWGN noise [default=%default]")
-    parser.add_option("-f", "--frequency-offset", type="eng_float", default=0,
-                      help="set frequency offset introduced by channel [default=%default]")
-    parser.add_option("-t", "--time-offset", type="eng_float", default=1.0,
-                      help="set timing offset between Tx and Rx [default=%default]")
-    parser.add_option("-p", "--phase-offset", type="eng_float", default=0,
-                      help="set phase offset (in degrees) between Tx and Rx [default=%default]")
-    parser.add_option("-m", "--use-multipath", action="store_true", default=False,
-                      help="Use a multipath channel [default=%default]")
-    parser.add_option("", "--tx-amplitude", type="eng_float",
-                      default=1.0,
-                      help="tell the simulator the signal amplitude [default=%default]")
+    parser = ArgumentParser(conflict_handler="resolve")
+    parser.add_argument("-n", "--snr", type=eng_float, default=30,
+                      help="set the SNR of the channel in dB [default=%(default)r]")
+    parser.add_argument("--seed", action="store_true", default=False,
+                      help="use a random seed for AWGN noise [default=%(default)r]")
+    parser.add_argument("-f", "--frequency-offset", type=eng_float, default=0,
+                      help="set frequency offset introduced by channel [default=%(default)r]")
+    parser.add_argument("-t", "--time-offset", type=eng_float, default=1.0,
+                      help="set timing offset between Tx and Rx [default=%(default)r]")
+    parser.add_argument("-p", "--phase-offset", type=eng_float, default=0,
+                      help="set phase offset (in degrees) between Tx and Rx [default=%(default)r]")
+    parser.add_argument("-m", "--use-multipath", action="store_true", default=False,
+                      help="Use a multipath channel [default=%(default)r]")
+    parser.add_argument("--tx-amplitude", type=eng_float, default=1.0,
+                      help="tell the simulator the signal amplitude [default=%(default)r]")
+    parser.add_argument("input_file", metavar="INPUT-FILE", nargs=1)
+    parser.add_argument("output_file", metavar="OUTPUT-FILE", nargs=1)
 
-    (options, args) = parser.parse_args ()
+    args = parser.parse_args()
 
-    if len(args) != 2:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+    ifile = args.input_file[0]
+    ofile = args.output_file[0]
 
-    ifile = args[0]
-    ofile = args[1]
-        
     # build the graph
-    tb = my_top_block(ifile, ofile, options)
+    tb = my_top_block(ifile, ofile, args)
 
     r = gr.enable_realtime_scheduling()
     if r != gr.RT_OK:
