7d4476de0cb80baa50f51d0a469761629b36992a
Jiří Pinkava <j-pi@seznam.cz>
2016-06-26 13:04:37 +0200

    gr-digital/examples/narrowband/benchmark_tx.py

--- gr-digital/examples/narrowband/benchmark_tx.py
+++ gr-digital/examples/narrowband/benchmark_tx.py
@@ -23,8 +23,8 @@
 from gnuradio import gr
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio.eng_option import eng_option
-from optparse import OptionParser
+from gnuradio.eng_arg import eng_float, intx
+from argparse import ArgumentParser
 
 # From gr-digital
 from gnuradio import digital
@@ -43,21 +43,21 @@ class my_top_block(gr.top_block):
     def __init__(self, modulator, options):
         gr.top_block.__init__(self)
 
-        if(options.tx_freq is not None):
+        if(args.tx_freq is not None):
             # Work-around to get the modulation's bits_per_symbol
             args = modulator.extract_kwargs_from_options(options)
-            symbol_rate = options.bitrate / modulator(**args).bits_per_symbol()
+            symbol_rate = args.bitrate / modulator(**args).bits_per_symbol()
 
-            self.sink = uhd_transmitter(options.args, symbol_rate,
-                                        options.samples_per_symbol, options.tx_freq,
-                                        options.lo_offset, options.tx_gain,
-                                        options.spec, options.antenna,
-                                        options.clock_source, options.verbose)
-            options.samples_per_symbol = self.sink._sps
+            self.sink = uhd_transmitter(args.args, symbol_rate,
+                                        args.samples_per_symbol, args.tx_freq,
+                                        args.lo_offset, args.tx_gain,
+                                        args.spec, args.antenna,
+                                        args.clock_source, args.verbose)
+            args.samples_per_symbol = self.sink._sps
             
-        elif(options.to_file is not None):
-            sys.stderr.write(("Saving samples to '%s'.\n\n" % (options.to_file)))
-            self.sink = blocks.file_sink(gr.sizeof_gr_complex, options.to_file)
+        elif(args.to_file is not None):
+            sys.stderr.write(("Saving samples to '%s'.\n\n" % (args.to_file)))
+            self.sink = blocks.file_sink(gr.sizeof_gr_complex, args.to_file)
         else:
             sys.stderr.write("No sink defined, dumping samples to null sink.\n\n")
             self.sink = blocks.null_sink(gr.sizeof_gr_complex)
@@ -79,42 +79,38 @@ def main():
 
     mods = digital.modulation_utils.type_1_mods()
 
-    parser = OptionParser(option_class=eng_option, conflict_handler="resolve")
-    expert_grp = parser.add_option_group("Expert")
+    parser = ArgumentParser(conflict_handler="resolve")
+    expert_grp = parser.add_argument_group("Expert")
 
-    parser.add_option("-m", "--modulation", type="choice", choices=mods.keys(),
+    parser.add_argument("-m", "--modulation", type="choice", choices=mods.keys(),
                       default='psk',
-                      help="Select modulation from: %s [default=%%default]"
+                      help="Select modulation from: %s [default=%%(default)r]"
                             % (', '.join(mods.keys()),))
 
-    parser.add_option("-s", "--size", type="eng_float", default=1500,
-                      help="set packet size [default=%default]")
-    parser.add_option("-M", "--megabytes", type="eng_float", default=1.0,
-                      help="set megabytes to transmit [default=%default]")
-    parser.add_option("","--discontinuous", action="store_true", default=False,
+    parser.add_argument("-s", "--size", type=eng_float, default=1500,
+                      help="set packet size [default=%(default)r]")
+    parser.add_argument("-M", "--megabytes", type=eng_float, default=1.0,
+                      help="set megabytes to transmit [default=%(default)r]")
+    parser.add_argument("--discontinuous", action="store_true", default=False,
                       help="enable discontinous transmission (bursts of 5 packets)")
-    parser.add_option("","--from-file", default=None,
+    parser.add_argument("--from-file", default=None,
                       help="use intput file for packet contents")
-    parser.add_option("","--to-file", default=None,
+    parser.add_argument("--to-file", default=None,
                       help="Output file for modulated samples")
 
-    transmit_path.add_options(parser, expert_grp)
-    uhd_transmitter.add_options(parser)
+    transmit_path.add_arguments(parser, expert_grp)
+    uhd_transmitter.add_arguments(parser)
 
     for mod in mods.values():
-        mod.add_options(expert_grp)
+        mod.add_arguments(expert_grp)
 
-    (options, args) = parser.parse_args ()
+    args = parser.parse_args()
 
-    if len(args) != 0:
-        parser.print_help()
-        sys.exit(1)
-           
-    if options.from_file is not None:
-        source_file = open(options.from_file, 'r')
+    if args.from_file is not None:
+        source_file = open(args.from_file, 'r')
 
     # build the graph
-    tb = my_top_block(mods[options.modulation], options)
+    tb = my_top_block(mods[args.modulation], options)
 
     r = gr.enable_realtime_scheduling()
     if r != gr.RT_OK:
@@ -123,13 +119,13 @@ def main():
     tb.start()                       # start flow graph
         
     # generate and send packets
-    nbytes = int(1e6 * options.megabytes)
+    nbytes = int(1e6 * args.megabytes)
     n = 0
     pktno = 0
-    pkt_size = int(options.size)
+    pkt_size = int(args.size)
 
     while n < nbytes:
-        if options.from_file is None:
+        if args.from_file is None:
             data = (pkt_size - 2) * chr(pktno & 0xff) 
         else:
             data = source_file.read(pkt_size - 2)
@@ -140,7 +136,7 @@ def main():
         send_pkt(payload)
         n += len(payload)
         sys.stderr.write('.')
-        if options.discontinuous and pktno % 5 == 4:
+        if args.discontinuous and pktno % 5 == 4:
             time.sleep(1)
         pktno += 1
         
